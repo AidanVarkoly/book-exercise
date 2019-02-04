18d765abee11b331dc58abb6e3c568266b059763
Aidan Varkoly <47114022+AidanVarkoly@users.noreply.github.com>
2019-02-04 12:44:20 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,24 +3,67 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Aidan Varkoly
+ * @version 2/4/19
  */
 class Book
 {
      // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String number;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        number = "";
     }
 
     // Add the methods here ...
-}
+    public String getAuthor()
+    {
+       return author;
+    }
+    
+    public String getTitle()
+    {
+       return title;
+    }
+    
+    public String getRefNumber()
+    {
+        return number;
+    }
+    public void printAuthor()
+    {
+        System.out.print(author);
+    }   
+    
+    public void printTitle()
+    {
+        System.out.print(title);
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }    
+    
+    public void printDetails()
+    {
+        System.out.print("Title: "+ title + "Author: "+ author + 
+        "Pages: " + pages + "Refrence Number: " +number);
+    }
+    public void setRefNumber(String ref)
+    {
+        System.out.print(number);
+    }    
+} 
+    
